4fd729c06a00bf3dfa9359208eb6f5cb13c7ab4c
Paul Curry <pcurry@gmail.com>
2015-05-27 17:44:35 -0400

    Extensions of existing functionality.

---
+++ file-system/watcher-hw.js
@@ -0,0 +1,20 @@
+"use strict";
+const
+    fs = require('fs'),
+    spawn = require('child_process').spawn,
+    watcher_child = process.argv[2];
+
+fs.watch(filename, function() {
+    let
+        ls = spawn('ls', ['-lh', filename]),
+        output = '';
+    ls.stdout.on('data', function(chunk){
+        output += chunk.toString();
+    });
+    ls.on('close', function(){
+        let parts = output.split(/\s+/);
+        console.dir([parts[0], parts[4], parts[8]]);
+    });
+});
+
+console.log("Now watching " + filename + " for changes...");
